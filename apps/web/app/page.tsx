a9118f36e0fde2b96fe64170e56ecf63a5194e38
Sven Nijhuis <sanijhuis@live.nl>
2023-03-02 13:41:50 +0100

    add next 13, tailwind wip

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
 import { Button } from "ui";
 
-export default function Docs() {
+export default function Page() {
   return (
     <div>
-      <h1>Docs</h1>
+      <h1>Web</h1>
       <Button />
     </div>
   );
---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    // app content
+    `app/**/*.{js,ts,jsx,tsx}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
