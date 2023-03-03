4aec5b5abd58627cf199b7f03137bc2c4ff7aae9
Sven Nijhuis <sanijhuis@live.nl>
2023-03-03 19:40:36 +0100

    Fix app folder

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Page() {
   return (
     <div>
-      <h1 className="text-red-500">Web</h1>
+      <h1 className="text-red-600 text-2xl font-bold">web</h1>
       <Button />
     </div>
   );
