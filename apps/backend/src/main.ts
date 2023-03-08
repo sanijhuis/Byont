ba749cf8a4a58d0889c2b4758de924324d684eff
Mees van Berkel <mees.van.berkel@hva.nl>
2023-03-08 21:37:41 +0100

    Added backend + prisma + auth service
    Port is 5000 and should be changed later on

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  //5000 is a temporary port
+  await app.listen(3000);
+}
+bootstrap();
