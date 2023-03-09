6062ec5f36df56a5a0983b6e188856af2b1ff909
Sven Nijhuis <sanijhuis@live.nl>
2023-03-09 13:01:01 +0100

    change localhost

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //5000 is a temporary port
-  await app.listen(5000);
+  //4000 is a temporary port
+  await app.listen(4000);
 }
 bootstrap();
