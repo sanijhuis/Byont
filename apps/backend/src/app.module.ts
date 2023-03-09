1cd83a85f7263207048de78bc7be4f8bcac71194
Sven Nijhuis <sanijhuis@live.nl>
2023-03-09 12:51:17 +0100

    delete docs and api folder

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
+import { AuthService } from './services/auth/auth.service';
 
 @Module({
   imports: [],
