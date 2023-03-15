0af421569e1b763dbb4656aa316a01700b2d3dd4
Lijkwaj <99690418+Lijkwaj@users.noreply.github.com>
2023-03-15 13:13:27 +0100

    Docker mythrill

---
+++ docker.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+exec('docker pull mythril/myth', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
