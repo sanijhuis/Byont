25173a0f1d22f56a85e41f5c11920490428aefe4
Lijkwaj <99690418+Lijkwaj@users.noreply.github.com>
2023-03-16 12:28:30 +0100

    scan mythril smart contract

---
+++ mythrill.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process');
+// Ping google three times
+exec('pip3 install mythril ', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+
+exec('myth analyze smart_contract.sol', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
