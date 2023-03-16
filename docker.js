c3944d51b223849c72305f945db1ef8ef6b1c86e
Lijkwaj <99690418+Lijkwaj@users.noreply.github.com>
2023-03-16 12:03:03 +0100

    Verbetering

--- docker.js
+++ docker.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('docker pull mythril/myth ', (error, stdout, stderr) => {
+exec('docker pull mythril/myth', (error, stdout, stderr) => {
   if (error) {
     console.error(`error: ${error.message}`);
     return;
