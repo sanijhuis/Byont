25f3af562f5610b2f1a0da1fe0008a5d24719b91
Lijkwaj <99690418+Lijkwaj@users.noreply.github.com>
2023-03-16 12:42:08 +0100

    makes docker container

--- docker.js
+++ docker.js
@@ -11,3 +11,15 @@ exec('docker pull mythril/myth', (error, stdout, stderr) => {
   }
   console.log(`stdout: ${stdout}`);
 });
+
+exec('docker run --publish 8080:80 --detach mythril/myth', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
