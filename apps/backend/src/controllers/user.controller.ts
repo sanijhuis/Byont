9573b5ce18a857299237fb5f94dbfa11c1f4e0c7
Mees van Berkel <mees.van.berkel@hva.nl>
2023-03-27 20:52:13 +0200

    Added relationship(user, repo), added sample code to usercontroller

--- apps/backend/src/controllers/user.controller.ts
+++ apps/backend/src/controllers/user.controller.ts
@@ -9,9 +9,39 @@ export class UsersController {
         this.prisma = new PrismaClient();
     }
 
+    async sampleCode() {
+        // Create a new user
+        const user = await this.prisma.user.create({
+            data: {
+                email: 'example@email.com',
+                password: 'password'
+            }
+        })
+
+        // Create a new repo for the user
+        const repo = await this.prisma.repo.create({
+            data: {
+                userId: user.id
+            }
+        })
+
+        // Get all repos for a user
+        const userWithRepos = await this.prisma.user.findUnique({
+            where: {
+                id: user.id
+            },
+            include: {
+                repos: true
+            }
+        })
+
+    }
+
     @Post()
     async createUser(@Body() data: { email: string, password: string }) {
         const user = await this.prisma.user.create({ data });
         return user;
     }
+
+
 }
