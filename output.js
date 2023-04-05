6bbd5a04f1357f5e40456c14734ce461251d3a21
Eyüb Cihan <eyub.cihan@gmail.com>
2023-04-05 15:36:43 +0200

    update 2

--- output.js
+++ output.js
@@ -1,27 +1,25 @@
 const { exec } = require('child_process');
 const readline = require('readline');
 
-// create a readline interface to ask for user input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-// ask for user input
-rl.question('What is your contract name?  ', (name) => {
-  // run the shell script and pass the user's name as an argument
-  exec(`./bash.sh ${name}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+rl.question('What is the name of the shell script? ', (scriptName) => {
+  rl.question('What is your name? ', (name) => {
+    exec(`./${scriptName} ${name}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
 
-  // close the readline interface
-  rl.close();
+    rl.close();
+  });
 });
