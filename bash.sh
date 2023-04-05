b50f8529cb7c342de183710660ff4cef40c1a2cb
Eyüb Cihan <eyub.cihan@gmail.com>
2023-04-05 14:53:55 +0200

    Update output.js

--- bash.sh
+++ bash.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-mkdir ScannerResults
-echo Enter the name of the solidity file:
-read solidity_filename
-myth analyze $solidity_filename 2>&1 | tee ScannerResults/scanner.txt
+
+echo "Hello, $1!"
--- output.js
+++ output.js
@@ -1,13 +1,27 @@
 const { exec } = require('child_process');
-// Ping google three times
-exec('./bash.sh', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
+const readline = require('readline');
+
+// create a readline interface to ask for user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// ask for user input
+rl.question('What is your name? ', (name) => {
+  // run the shell script and pass the user's name as an argument
+  exec(`./bash.sh ${name}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+
+  // close the readline interface
+  rl.close();
 });
