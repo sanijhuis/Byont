f66d0c8436635f4e9d97d6906620911a2da211e1
KortSam <samkort@hotmail.nl>
2023-04-22 19:36:17 +0200

    added Slither

--- apps/backend/src/controllers/file.controller.ts
+++ apps/backend/src/controllers/file.controller.ts
@@ -25,19 +25,67 @@ export class FileController {
       }),
     }),
   )
-  async analyzeFile(@UploadedFile() file: Express.Multer.File) {
+  // async analyzeFile(@UploadedFile() file: Express.Multer.File) {
+  //   try {
+  //     console.log(file.filename);
+
+  //     const docker = new Docker();
+  //     const container = await docker.createContainer({
+  //       HostConfig: {
+  //         Binds: [`${process.cwd()}/uploads:/mnt`],
+  //       },
+
+  //       Image: 'mythril/myth:latest',
+  //       Cmd: ['analyze', `/mnt/${file.filename}`],
+
+
+  //     });
+
+
+  //     await container.start();
+
+  //     const logs = await container.logs({
+  //       follow: true,
+  //       stdout: true,
+  //       stderr: true,
+  //     });
+
+  //     let logsData = '';
+  //     logs.pipe(process.stdout);
+  //     logs.on('data', async(data) => {
+  //        logsData += data;
+  //     });
+
+
+
+
+
+  //     logs.on('end', async() => {
+  //       container.remove({ force: true });
+  //     });
+
+  //   } catch (err) {
+  //     console.error('Error creating or starting container:', err);
+  //   }
+  // }
+
+  async analyzeSlither(@UploadedFile() file: Express.Multer.File) {
     try {
       console.log(file.filename);
 
       const docker = new Docker();
       const container = await docker.createContainer({
-        Image: 'mythril/myth:latest',
-        Cmd: ['analyze', `/mnt/${file.filename}`],
-
         HostConfig: {
           Binds: [`${process.cwd()}/uploads:/mnt`],
         },
+
+        Image: 'trailofbits/slither:latest',
+        Cmd: ['slither', `/mnt/${file.filename}`],
+
+
+
       });
+
 
       await container.start();
 
@@ -49,16 +97,26 @@ export class FileController {
 
       let logsData = '';
       logs.pipe(process.stdout);
-      logs.on('data', (data) => {
+      logs.on('data', async (data) => {
         logsData += data;
       });
 
-      logs.on('end', () => {
-        console.log('Logs:', logsData);
+
+
+
+      logs.on('end', async () => {
         container.remove({ force: true });
+
       });
+
     } catch (err) {
       console.error('Error creating or starting container:', err);
     }
   }
 }
+
+
+
+
+
+
--- apps/backend/uploads/contract.sol
+++ apps/backend/uploads/contract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.4.25;
 contract HelloWorld {
     function sayHelloWorld() public pure returns (string memory) {
         return "Hello World";
