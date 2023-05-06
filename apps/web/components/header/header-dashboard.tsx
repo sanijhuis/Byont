746b6a68c59d772e376a32067af840db7940f406
svennijhuis <sanijhuis@live.nl>
2023-05-06 17:40:24 +0200

    wip

--- apps/web/components/header/header-dashboard.tsx
+++ apps/web/components/header/header-dashboard.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 type HeaderDashboardProps = React.ComponentProps<"div"> & {};
@@ -18,9 +17,8 @@ type tabs = {
   label: string;
 };
 
-export default function HeaderDashboard({}: HeaderDashboardProps) {
-  const pathname = usePathname();
-
+const HeaderDashboard = ({}: HeaderDashboardProps) => {
+  let [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
     <div className="sticky top-6 h-5 w-screen gap-2 bg-black">
       <nav className="container flex h-full w-full items-center">
@@ -28,17 +26,19 @@ export default function HeaderDashboard({}: HeaderDashboardProps) {
           <Link
             href={tab.id}
             key={tab.id}
+            onMouseEnter={() => setActiveTab(tab.id)}
+            onMouseLeave={() => setActiveTab("")}
             className={`${
-              pathname === tab.id ? "" : "hover:text-white/60"
+              activeTab === tab.id ? "" : "hover:text-white/60"
             } relative rounded-md px-2 py-[5px] text-[14px] font-normal text-white outline-sky-400 transition focus-visible:outline-2`}
             style={{
               WebkitTapHighlightColor: "transparent",
             }}
           >
-            {pathname === tab.id && (
+            {activeTab === tab.id && (
               <motion.span
                 layoutId="bubble"
-                className="absolute inset-0 z-10 bg-white mix-blend-difference"
+                className="absolute inset-0 z-10 bg-white mix-blend-difference "
                 style={{ borderRadius: 10 }}
                 transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
               />
@@ -49,4 +49,6 @@ export default function HeaderDashboard({}: HeaderDashboardProps) {
       </nav>
     </div>
   );
-}
+};
+
+export default HeaderDashboard;
