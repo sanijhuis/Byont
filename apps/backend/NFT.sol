f38c8ab20548519c92911129806f9381d70de7d7
Jaïr Lijkwan <jair.lijkwan@hva.nl>
2023-05-29 13:12:15 +0200

    Add new file

---
+++ apps/backend/NFT.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public mintRate = 0.01 ether;
+    uint public MAX_SUPPLY = 10000;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://api.mynft.com/tokens/";
+    }
+
+    function safeMint(address to) public payable {
+        require(totalSupply() < MAX_SUPPLY, "Can's mint more.");
+        require(msg.value >= mintRate, "Not enough ether sent.");
+        _tokenIdCounter.increment();
+        _safeMint(to, _tokenIdCounter.current());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function withdraw() public onlyOwner {
+        require(address(this).balance > 0, "Balance is 0");
+        payable(owner()).transfer(address(this).balance);
+    }
+}
