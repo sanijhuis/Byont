2a24bceedd40e1061bb1bc61e321147e501a083a
Mees van Berkel <mees.van.berkel@hva.nl>
2023-06-04 23:41:12 +0200

    unneeded comments

--- apps/backend/src/services/scanResult.service.ts
+++ apps/backend/src/services/scanResult.service.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class ScanResultService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   async findOwnerByRepoName(repoName: string): Promise<string | null> {
     const repo = await this.prisma.repo.findFirst({
@@ -15,7 +15,7 @@ export class ScanResultService {
         owner: true,
       },
     });
-  
+
     return repo ? repo.owner : null;
   }
 
@@ -23,32 +23,31 @@ export class ScanResultService {
   async getScanOutputByRepoNameAndId(repoName: string, id: number) {
     // Convert the id parameter to a number
     const numId = Number(id);
-   
-    // If the conversion is unsuccessful, throw an error
-    if(isNaN(numId)) {
+
+    if (isNaN(numId)) {
       throw new Error('Invalid id value');
     }
-  
-    const ownerName = await this.findOwnerByRepoName(repoName);
-if (!ownerName) {
-  throw new Error('No repository found with the given name');
-}
 
-    // Find the repo with the given repoName
+    const ownerName = await this.findOwnerByRepoName(repoName);
+    if (!ownerName) {
+      throw new Error('No repository found with the given name');
+    }
+
+    
     const repo = await this.prisma.repo.findUnique({
       where: {
         name_owner: {
           name: repoName,
-          owner: ownerName, // you need to have the ownerName
+          owner: ownerName, 
         },
       },
     });
-  
-    // If no repo is found, throw an error
+
+   
     if (!repo) {
       throw new Error('No repository found with the given name');
     }
-  
+
     // Find the scan output with the given id and repoId
     const scanOutput = await this.prisma.scanOutput.findFirst({
       where: {
@@ -56,19 +55,19 @@ if (!ownerName) {
         repoId: repo.id,
       },
       include: {
-        scanOutputItems: true, // Include ScanOutputItems in the response
+        scanOutputItems: true, 
       },
     });
-  
+
     // If no scan output is found, throw an error
     if (!scanOutput) {
-      throw new Error('No scan output found with the given id for this repository');
+      throw new Error(
+        'No scan output found with the given id for this repository'
+      );
     }
-  
+
     return scanOutput;
   }
-  
-  
 
   async getScanOutputsByRepoName(repoName: string) {
     return this.prisma.scanOutput.findMany({
